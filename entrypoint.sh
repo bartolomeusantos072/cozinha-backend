4673b29dce46d90c62899dc8296ec480c4d7b65b
Bartolomeu Uender <bartolomeusantos072@gmail.com>
2025-06-25 14:59:05 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,6 @@ if [ -f /etc/secrets/.env ]; then
   cp /etc/secrets/.env .env
 fi
 
-echo "Building project..."
-npm run build
-
 echo "Running Prisma generate..."
 npx prisma generate
 
@@ -15,7 +12,7 @@ echo "Running migrations..."
 npx prisma migrate deploy
 
 echo "Running seed..."
-node dist/prisma/seed.js  # roda o JS compilado, que já foi gerado no build
+node dist/prisma/seed.js 
 
 echo "Starting server..."
 exec node dist/index.js
